ea8559ca93f771250961a63fbe0f7acc3a7a2338
sourceperl <loic.celine@free.fr>
2016-07-21 16:56:01 +0200

    add dnsmasq config

--- setup.sh
+++ setup.sh
@@ -3,13 +3,17 @@
 # add to Raspbian
 sudo apt-get install -y hostapd dnsmasq ipython python-picamera vim git nmap supervisor
 
-# config access-point
+# config hostapd (set internal wifi chip to access-point mode)
 sudo cp etc/network/interfaces /etc/network/interfaces
 sudo cp etc/default/hostapd /etc/default/hostapd
 sudo cp etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf
 sudo service hostapd start
 
-# add Python/Flask web video streamer
+# config dnsmasq (provide DHCP server and DNS service to wifi client)
+sudo cp etc/dnsmasq.conf /etc/dnsmasq.conf
+sudo service dnsmasq start
+
+# add Python/Flask web video streamer (provide MJPEG and HTTP)
 mkdir /home/pi/utils
 cp -r ./flask-video-streaming/ /home/pi/utils
 
